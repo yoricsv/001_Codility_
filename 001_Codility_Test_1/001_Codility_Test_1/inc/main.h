531748a71a1a94b3c585fb75839cea910a8458f0
Сергей <yoric@live.ru>
2022-02-18 12:46:26 +0300

    Add README.md and gitignore

---
+++ 001_Codility_Test_1/001_Codility_Test_1/inc/main.h
@@ -0,0 +1,10 @@
+//
+// Created by Yoric on 17-Feb-22.
+//
+
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <iostream>
+
+#endif //_MAIN_H
---
+++ 001_Codility_Test_1/001_Codility_Test_1/src/main.cpp
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main(
+        int    argc,
+        char * argv[]
+    )
+{
+    std::cout << "Hello, World!" << std::endl;
+
+    return std::cout.good() ? EXIT_SUCCESS : EXIT_FAILURE;
+}
